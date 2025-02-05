3ebecc95485738a9df18d6231c74cfad55cc1ed8
Ashraf Saeed <dr.ashrafsaeed99@gmail.com>
2025-02-05 17:25:34 +0300

    create and add to git

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
